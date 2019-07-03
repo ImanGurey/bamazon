87c714bfbdceed2d4e8562f14ee53dfc3dea38f4
Iman Gure <imangu99@gmail.com>
2019-07-02 18:45:05 -0700

    updating

--- bamazon-db.sql
+++ bamazon-db.sql
@@ -1,19 +1,21 @@
 DROP DATABASE IF EXISTS bamazon_db;
+
 CREATE DATABASE bamazon_db;
+
 USE bamazon_db;
 
 CREATE TABLE products
 (
-    item_id INT
-    AUTO_INCREMENT NOT NULL,
-    department_id  INT NOT NULL,
-    product_name   VARCHAR
-    (100) NOT NULL,
-    price          DECIMAL
-    (8, 2) NOT NULL,
-    stock_quantity INT NOT NULL,
-    product_sales  DECIMAL
-    (8, 2) NOT NULL DEFAULT 0,
+    item_id INT NOT NULL
+    AUTO_INCREMENT,
+	product_name VARCHAR
+    (255) NULL,
+	department_name VARCHAR
+    (255) NULL,
+	price DECIMAL
+    (10,2) NULL,
+	stock_quantity INT
+    (7),
     PRIMARY KEY
     (item_id)
-);
+)
--- bamazon-seeds.sql
+++ bamazon-seeds.sql
@@ -1,1 +1,15 @@
+USE bamazon_db;
 
+INSERT INTO products
+    (product_name,item_id, department_name, price, stock_quantity)
+VALUES
+    (101,'Bentley 6V Ride-On by Dynacraft', 'Toys', 229.99, 896),
+    (212,'BOSU Balance Trainer & 2-Disc DVD Set', 'Sports', 139.99, 847),
+    (313,'JBL Flip 3 Splashproof Wireless Bluetooth Speaker', 'Electronics', 99.99, 2500),
+    (420,'Keurig K55 Coffee Brewing System', 'Kitchen', 119.99, 347),
+    (504,'Gaiam 6mm Geo Feather Reversible Yoga Mat', 'Sports', 34.99, 235),
+    (619,'Fiest 4-pc. Place Setting', 'Kitchen', 32.99, 295),
+    (720,'Leick Furniture Mesh Back Office Chair', 'Office', 110.49, 150),
+    (808,"Fisher-Price Laugh & Learn Singin'Soccer Ball", 'Toys', 16.09, 5000),
+    (913,'Beats Solo3 Wireless Headphones', 'Electronics', 299.99, 187),
+    (1009,'Marcy 30-lb. Kettlebell Kit', 'Sports', 74.99, 453);
